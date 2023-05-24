4e7e4e0d692ae15a70ad0180abd54a10f3a0f530
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-24 16:01:36 -0300

    adicionado funcionalidade de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/HomePage/HomePage";
 import SeatsPage from "./pages/SeatsPage/SeatsPage";
 import SessionsPage from "./pages/SessionsPage/SessionsPage";
@@ -6,14 +8,15 @@ import SuccessPage from "./pages/SuccessPage/SuccessPage";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavContainer>CINEFLEX</NavContainer>
-
-      <HomePage />
-      {/* <SeatsPage /> */}
-      {/* <SessionsPage /> */}
-      {/* <SuccessPage /> */}
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/seats" element={<SeatsPage />} />
+        <Route path="/sessions" element={<SessionsPage />} />
+        <Route path="/success" element={<SuccessPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
